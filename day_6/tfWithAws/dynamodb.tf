7f79eb58dd8f9bc9a902181d986b09ab0c6dba52
Sundze <keyonmwesundze@mail.com>
2024-01-09 01:28:48 +0100

    teraform form with aws - DynamoDB

--- day_6/tfWithAws/dynamodb.tf
+++ day_6/tfWithAws/dynamodb.tf
@@ -2,10 +2,28 @@ resource "aws_dynamodb_table" "users" {
   name = "users"
   hash_key = "UserID"
   billing_mode = "PAY_PER_REQUEST"
+  #primary key
   attribute {
     name = "UserID"
+    # S - string
     type = "S"
   }
 
   tags = local.common_tags
+}
+
+resource "aws_dynamodb_table_item" "car_items" {
+  table_name = aws_dynamodb_table.users.name
+  hash_key = aws_dynamodb_table.users.hash_key
+
+  for_each = {for user in local.users : user.UserID => user}
+
+  item = <<EOT
+  {
+    "UserID": {"S": "${each.value.UserID}"},
+    "Email": {"S": "${each.value.Email}"},
+    "Age": {"N": "${each.value.Age}"},
+    "Name": {"S": "${each.value.Name}"}      
+  }
+  EOT
 }
--- day_6/tfWithAws/locals.tf
+++ day_6/tfWithAws/locals.tf
@@ -8,4 +8,29 @@ locals {
     #you can call other locals within another local
     #Code = "${local.prefix}-${local.common_tags.Team}"
   }
+
+}
+
+locals {
+  users = [
+    {
+      UserID : "U2"
+      Email : "user1@example.com"
+      Name : "John Doe"
+      Age = 40
+    },
+    {
+      UserID : "U3"
+      Email : "user2@example.com"
+      Name : "Mary Awa"
+      Age = 24
+    },
+    {
+      UserID : "U4"
+      Email : "user3@example.com"
+      Name : "Mike Truce"
+      Age = 21
+    }
+
+  ]
 }
